8cf0c109738f7f68c6d250eb0642de1474a8f0ca
Roshan Ghale <ghaleroshan@gmail.com>
2019-08-03 19:41:34 +1000

    Adds pagination to currency listing GET API (#19)
    * Adds a pagination Limit of 10 rates per page
    
    * - [x] Adds pagination with the Limit of 10 per page
    - [x] Fetches page number from the index.js
    - [x] Adds test for the pagination
    
    * - [x] Moves itemsPerPage to config file
    - [x] removes unnecessary spaces
    - [x] Adds query ends with in the test file
    
    * - [x] Adds endsWith query the test
    
    * - [x] removes unnecessary lines
    
    * - [x] changes the local variables to exact numbers i.e. LIMIT 0 10
    
    * - [x] Adds env variable for itemsPerPage in config
    - [x] Uses parameterised query for allExchangeRates
    - [x] Fixes {} in exchangeRatesTest
    
    * - [x] Removes .value from itemsperpage config settings
    
    * - [x] Removes .idea from the .gitignore
    - [x] Adds extra line at the end of exchangeRates.js

--- test/exchnageRatesTest.js
+++ test/exchnageRatesTest.js
@@ -12,7 +12,7 @@ const today = new Date().toISOString().split('T')[0];
 
 describe('exchangeRates', () => {
   describe('get', () => {
-    it('should use given params and if no results are in db, inserts new rate to db while returing it', async () => {
+    it('should use given params and if no results are in db, inserts new rate to db while returning it', async () => {
       try {
         const onDate = '2018-12-03';
         mysqlStub.query = (query, params) => {
@@ -20,7 +20,7 @@ describe('exchangeRates', () => {
             return [];
           }
 
-          if(query.startsWith(`INSERT INTO exchange_rates`)) {
+          if (query.startsWith(`INSERT INTO exchange_rates`)) {
             assert.ok(query.includes(`(from_currency, to_currency, rate, on_date) VALUES (?,?,?,?)`))
             assert.ok(query.includes(`ON DUPLICATE KEY UPDATE rate = ?`))
             assert.deepEqual(params, ['AUD', 'USD', 0.742725, onDate, 0.742725]);
@@ -30,7 +30,7 @@ describe('exchangeRates', () => {
               insertId: 5,
               info: '',
               serverStatus: 2,
-              warningStatus: 0 
+              warningStatus: 0
             });
           }
         };
@@ -40,11 +40,11 @@ describe('exchangeRates', () => {
         nock('https://free.currconv.com').get(/api\/v7\/convert\?q=*/).reply(200, apiResponse);
 
         const result = await exchangeRates.get({fromCurrency: 'AUD', toCurrency: 'USD', onDate});
-        assert.deepEqual(result, { 
+        assert.deepEqual(result, {
           fromCurrency: 'AUD',
           toCurrency: 'USD',
           onDate: '2018-12-03',
-          rate: 0.742725 
+          rate: 0.742725
         });
       } catch (err) {
         console.log(`err`, err);
@@ -58,16 +58,16 @@ describe('exchangeRates', () => {
           if (query.startsWith('SELECT rate, created_at FROM exchange_rates')) {
             assert.ok(query.includes(`WHERE from_currency = ? AND to_currency = ? AND on_date = ?`));
             assert.deepEqual(params, ['AUD', 'USD', today], 'fromCurrency, then toCurrency then onDate respectively');
-            return [{ rate: '0.7427190', created_at: '2018-07-23T06:58:45.000Z'}];
+            return [{rate: '0.7427190', created_at: '2018-07-23T06:58:45.000Z'}];
           }
-        };        
+        };
 
         const result = await exchangeRates.get({});
-        assert.deepEqual(result, { 
+        assert.deepEqual(result, {
           fromCurrency: 'AUD',
           toCurrency: 'USD',
           onDate: today,
-          rate: 0.7427190 
+          rate: 0.7427190
         });
       } catch (err) {
         console.log(`err`, err);
@@ -80,19 +80,19 @@ describe('exchangeRates', () => {
         mysqlStub.query = (query, params) => {
           if (query.startsWith(`SELECT rate, created_at FROM exchange_rates`)) {
             return [];
-          }         
+          }
         };
         let badResponse = {
           status: 400,
           error: "Currency ASD is unavailable from 2018-10-05 to 2018-10-05"
-        };        
+        };
         nock('https://free.currconv.com').get(/api*/).reply(400, badResponse);
 
         const result = await exchangeRates.get({fromCurrency: 'ASD', toCurrency: 'AUD', onDate: '2018-10-05'});
         assert.equal(err.message, 'should never reach here');
       } catch (err) {
         assert.equal(err.statusCode, 400);
-        assert.equal(err.message, 'Currency ASD is unavailable from 2018-10-05 to 2018-10-05');        
+        assert.equal(err.message, 'Currency ASD is unavailable from 2018-10-05 to 2018-10-05');
       }
     });
 
@@ -101,16 +101,32 @@ describe('exchangeRates', () => {
         mysqlStub.query = (query, params) => {
           if (query.startsWith(`SELECT rate, created_at FROM exchange_rates`)) {
             return [];
-          }         
+          }
         };
-        let wrongApiResponse = {'AUD_USD': {}};        
+        let wrongApiResponse = {'AUD_USD': {}};
         nock('https://free.currconv.com').get(/api*/).reply(200, wrongApiResponse);
         const result = await exchangeRates.get({fromCurrency: 'USD', toCurrency: 'AUD', onDate: '2018-07-21'});
         assert.equal(err.message, 'should never reach here');
       } catch (err) {
         assert.equal(err.statusCode, 400);
-        assert.equal(err.message, 'Error in fetching rate');        
+        assert.equal(err.message, 'Error in fetching rate');
       }
     });
+      it('should paginate the result with the limit of 10 rows per page', async () => {
+        try {
+          mysqlStub.query = (query, params) => {
+            if (query.startsWith(`SELECT from_currency, to_currency`)
+                && query.endsWith(`LIMIT 0 10`)) {
+              return [];
+            }
+          };
+          const result = await exchangeRates.getMultiple({});
+          assert.deepStrictEqual(result, []);
+        } catch (err) {
+          console.log('err',err);
+          assert.deepStrictEqual(err.message, 'Error in connecting to database');
+        }
+      });
+    });
   });
-});
+
