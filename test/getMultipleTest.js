5243cc0260cb28372250b7113322f91d7de35ce2
Roshan Ghale <ghaleroshan@gmail.com>
2019-08-07 14:51:18 +1000

    Fixing spaces in getMultiple Test
    - [x] fixes spaces

--- test/getMultipleTest.js
+++ test/getMultipleTest.js
@@ -11,47 +11,47 @@ const exchangeRates = proxyquire(
 const today = new Date().toISOString().split('T')[0];
 
 describe('exchangeRates', () => {
-    describe('get', () => {
-        it("should return no rates from the db when no rates exists", async () => {
-            try {
-                mysqlStub.query = (query, params) => {
-                    if (query.startsWith(`SELECT from_currency, to_currency`)) {
-                        return [];
-                    }
-                };
-
-                const result = await exchangeRates.getMultiple({});
-                assert.deepStrictEqual(result, []);
-            } catch (err) {
-                console.log(`err`, err);
-                assert.strictEqual(err.message, "should never reach here");
+  describe('get', () => {
+    it("should return no rates from the db when no rates exists", async () => {
+      try {
+        mysqlStub.query = (query, params) => {
+          if (query.startsWith(`SELECT from_currency, to_currency`)) {
+            return [];
             }
-        });
-        it("should return exchange rates from the db when exchange rates exists", async () => {
-            try {
-                mysqlStub.query = (query, params) => {
-                    if (query.startsWith(`SELECT from_currency, to_currency`)) {
-                        return [
-                            {
-                                from_currency:"USD",
-                                to_currency:"AUD",
-                                rate:1.4374030,
-                                on_date:"2019-06-10T14:00:00.000Z"
-                            }
-                        ];
-                    }
-                };
+            };
 
-                const result = await exchangeRates.getMultiple({});
-                assert.deepStrictEqual(result[0], {
-                    from_currency:"USD",
-                    to_currency:"AUD",
-                    rate:1.4374030,
-                    on_date:"2019-06-10T14:00:00.000Z"
-                });
+            const result = await exchangeRates.getMultiple({});
+            assert.deepStrictEqual(result, []);
             } catch (err) {
-                console.log(`err`, err);
-                assert.deepStrictEqual(err.message, "should never reach here");
+            console.log(`err`, err);
+            assert.strictEqual(err.message, "should never reach here");
+            }
+    });
+    it("should return exchange rates from the db when exchange rates exists", async () => {
+      try {
+        mysqlStub.query = (query, params) => {
+          if (query.startsWith(`SELECT from_currency, to_currency`)) {
+            return [
+              {
+                from_currency:"USD",
+                to_currency:"AUD",
+                rate:1.4374030,
+                on_date:"2019-06-10T14:00:00.000Z"
+              }
+            ];
+          }
+        };
+
+          const result = await exchangeRates.getMultiple({});
+            assert.deepStrictEqual(result[0], {
+                from_currency:"USD",
+                to_currency:"AUD",
+                rate:1.4374030,
+                on_date:"2019-06-10T14:00:00.000Z"
+            });
+          } catch (err) {
+            console.log(`err`, err);
+            assert.deepStrictEqual(err.message, "should never reach here");
             }
         });
     });
