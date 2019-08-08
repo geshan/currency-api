8e9541ae08c18787d0629b2043c727e466a93ec1
Roshan Ghale <ghaleroshan@gmail.com>
2019-08-08 15:09:09 +1000

    Adds Husky as pre-commit hooks
    - [x] Adds husky as a pre-commit hooks
    - [x] fixes indentation

--- test/getMultipleTest.js
+++ test/getMultipleTest.js
@@ -1,31 +1,30 @@
-const assert = require('assert');
-const nock = require('nock');
-const proxyquire = require('proxyquire').noCallThru();
+const assert = require("assert");
+const nock = require("nock");
+const proxyquire = require("proxyquire").noCallThru();
 
 const mysqlStub = {};
 
-const exchangeRates = proxyquire(
-    './../src/exchangeRates',
-    { 'namshi-node-mysql': _ => mysqlStub },
-);
-const today = new Date().toISOString().split('T')[0];
+const exchangeRates = proxyquire("./../src/exchangeRates", {
+  "namshi-node-mysql": _ => mysqlStub
+});
+const today = new Date().toISOString().split("T")[0];
 
-describe('exchangeRates', () => {
-  describe('get', () => {
+describe("exchangeRates", () => {
+  describe("get", () => {
     it("should return no rates from the db when no rates exists", async () => {
       try {
         mysqlStub.query = (query, params) => {
           if (query.startsWith(`SELECT from_currency, to_currency`)) {
             return [];
-            }
-            };
+          }
+        };
 
-            const result = await exchangeRates.getMultiple({});
-            assert.deepStrictEqual(result, []);
-            } catch (err) {
-            console.log(`err`, err);
-            assert.strictEqual(err.message, "should never reach here");
-            }
+        const result = await exchangeRates.getMultiple({});
+        assert.deepStrictEqual(result, []);
+      } catch (err) {
+        console.log(`err`, err);
+        assert.strictEqual(err.message, "should never reach here");
+      }
     });
     it("should return exchange rates from the db when exchange rates exists", async () => {
       try {
@@ -33,26 +32,26 @@ describe('exchangeRates', () => {
           if (query.startsWith(`SELECT from_currency, to_currency`)) {
             return [
               {
-                from_currency:"USD",
-                to_currency:"AUD",
-                rate:1.4374030,
-                on_date:"2019-06-10T14:00:00.000Z"
+                from_currency: "USD",
+                to_currency: "AUD",
+                rate: 1.437403,
+                on_date: "2019-06-10T14:00:00.000Z"
               }
             ];
           }
         };
 
-          const result = await exchangeRates.getMultiple({});
-            assert.deepStrictEqual(result[0], {
-                from_currency:"USD",
-                to_currency:"AUD",
-                rate:1.4374030,
-                on_date:"2019-06-10T14:00:00.000Z"
-            });
-          } catch (err) {
-            console.log(`err`, err);
-            assert.deepStrictEqual(err.message, "should never reach here");
-            }
+        const result = await exchangeRates.getMultiple({});
+        assert.deepStrictEqual(result[0], {
+          from_currency: "USD",
+          to_currency: "AUD",
+          rate: 1.437403,
+          on_date: "2019-06-10T14:00:00.000Z"
         });
+      } catch (err) {
+        console.log(`err`, err);
+        assert.deepStrictEqual(err.message, "should never reach here");
+      }
     });
+  });
 });
